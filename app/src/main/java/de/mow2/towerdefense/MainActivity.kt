e8c84170555573517181681d25c686f7b063f0c4
dastrinken <61236723+dastrinken@users.noreply.github.com>
2022-10-05 12:10:32 +0200

    First commit, setting up Android Studio Project.
    Contains basic file structure and stuff.

---
+++ app/src/main/java/de/mow2/towerdefense/MainActivity.kt
@@ -0,0 +1,22 @@
+package de.mow2.towerdefense
+
+import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.os.Build
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.MotionEvent
+import android.view.View
+import android.view.WindowInsets
+import android.widget.LinearLayout
+import android.widget.TextView
+import de.mow2.towerdefense.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+    }
+}
