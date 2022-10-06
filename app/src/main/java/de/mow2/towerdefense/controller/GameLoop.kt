62d69d2bef9dafbeb5acbcabc0776fe2969104b4
dastrinken <61236723+dastrinken@users.noreply.github.com>
2022-10-06 13:23:01 +0200

    loop implemented

--- app/src/main/java/de/mow2/towerdefense/controller/GameLoop.kt
+++ app/src/main/java/de/mow2/towerdefense/controller/GameLoop.kt
@@ -1,16 +1,40 @@
 package de.mow2.towerdefense.controller
 
+import android.graphics.Canvas
 import android.view.SurfaceHolder
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import java.lang.Exception
 
 class GameLoop(val gameView: GameView, val surfaceHolder: SurfaceHolder) {
+    private var running = false
+
     fun startLoop() {
         runBlocking {
             launch {
-
+                while(running) {
+                    try {
+                        canvas = surfaceHolder.lockCanvas()
+                        gameView.update()
+                        gameView.draw(canvas)
+                    } catch (e: java.lang.Exception) {
+                        e.printStackTrace()
+                    } finally {
+                        if(canvas != null) {
+                            try {
+                                //draw canvas and post
+                                surfaceHolder.unlockCanvasAndPost(canvas)
+                            } catch (e: Exception) {
+                                e.printStackTrace()
+                            }
+                        }
+                    }
+                }
             }
         }
+    }
+
+    companion object {
+        private var canvas: Canvas? = null
     }
 }
--- app/src/main/java/de/mow2/towerdefense/controller/GameView.kt
+++ app/src/main/java/de/mow2/towerdefense/controller/GameView.kt
@@ -2,10 +2,13 @@ package de.mow2.towerdefense.controller
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Paint
+import android.util.AttributeSet
 import android.view.SurfaceHolder
 import android.view.SurfaceView
+import java.util.jar.Attributes
 
-class GameView(context: Context) : SurfaceView(context), SurfaceHolder.Callback {
+class GameView(context: Context, attributes: AttributeSet) : SurfaceView(context, attributes), SurfaceHolder.Callback {
     var gameLoop: GameLoop
 
     init {
@@ -19,18 +22,20 @@ class GameView(context: Context) : SurfaceView(context), SurfaceHolder.Callback 
     }
 
     override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
-        TODO("Not yet implemented")
+
     }
 
     override fun surfaceDestroyed(holder: SurfaceHolder) {
-        TODO("Not yet implemented")
+
     }
 
     fun update() {
-
     }
 
     override fun draw(canvas: Canvas?) {
         super.draw(canvas)
+        if (canvas != null) {
+            canvas.drawRect(100f, 100f, 100f, 100f, Paint().color )
+        }
     }
 }
