00f4793ed70c5f9473ef47666326a733706fd0a4
Rikerus <45260136+Rikerus@users.noreply.github.com>
2022-10-07 15:11:12 +0200

    erste styles anpassungen für main activity + erstellung eines hauptmenues (ohne funktion) + hintergrundmusik in main activity

--- app/src/main/java/de/mow2/towerdefense/MainActivity.kt
+++ app/src/main/java/de/mow2/towerdefense/MainActivity.kt
@@ -14,6 +14,8 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import de.mow2.towerdefense.controller.GameActivity
 import de.mow2.towerdefense.databinding.ActivityMainBinding
+import android.media.MediaPlayer
+import android.widget.ImageButton
 
 /**
  * Remove comment before Release!!!
@@ -23,6 +25,7 @@ import de.mow2.towerdefense.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        playBackgroundMusic()
 
         setContentView(R.layout.activity_main)
     }
@@ -30,4 +33,49 @@ class MainActivity : AppCompatActivity() {
     fun startGame(view: View) {
         startActivity(Intent(this, GameActivity::class.java))
     }
+
+    // tryout PopUp Window for Navigation
+
+
+
+
+    // background music in main activity
+    var backgroundMusic: MediaPlayer? = null
+
+    // 1. starts background music with app start
+    fun playBackgroundMusic() {
+        if (backgroundMusic == null) {
+            backgroundMusic = MediaPlayer.create(this, R.raw.sound1)
+            backgroundMusic!!.isLooping = true
+            backgroundMusic!!.start()
+        } else backgroundMusic!!.start()
+    }
+
+    // 2. pause || resumes playback
+    fun pauseResumeMusic(view: View) {
+        var pauseResumeButton = findViewById<ImageButton>(R.id.pause_resume_Button)
+        if (backgroundMusic?.isPlaying == true) {
+            backgroundMusic?.pause()
+            pauseResumeButton.setImageResource(androidx.appcompat.R.drawable.abc_ic_voice_search_api_material)
+        } else {
+            backgroundMusic?.start()
+            pauseResumeButton.setImageResource(androidx.appcompat.R.drawable.abc_ic_go_search_api_material)
+        }
+    }
+
+    // 3. restart playback when reentering main activity
+    override fun onRestart() {
+        super.onRestart()
+        playBackgroundMusic()
+    }
+
+    // 4. destroys MediaPlayer instance when the app is closed
+    override fun onStop() {
+        super.onStop()
+        if (backgroundMusic != null) {
+            backgroundMusic!!.release()
+            backgroundMusic = null
+        }
+    }
+
 }
