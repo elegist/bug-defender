4e2a680e4460791912d2b6bdb98952f79eadd120
dastrinken <61236723+dastrinken@users.noreply.github.com>
2022-10-09 10:41:31 +0200

    Update PlayGround.kt

--- app/src/main/java/de/mow2/towerdefense/model/playground/PlayGround.kt
+++ app/src/main/java/de/mow2/towerdefense/model/playground/PlayGround.kt
@@ -7,27 +7,25 @@ import de.mow2.towerdefense.R
 class PlayGround(val width: Int, val height: Int, resources: Resources) {
     val TAG = javaClass.name
     var squareArray = emptyArray<SquareField>()
-    private val squaresX = 10
-    private val squaresY = 20
+    private val squaresX = 9
+    private val squaresY = 18
     init {
         val squareWidth = width / squaresX
         val squareHeight = height / squaresY
-        var posX = 0
-        var posY: Int
-        var squareId = 0
-        for(i in 0..squaresX) {
-            posY = 0
-            for(j in 0..squaresY) {
+        var posY = 0
+        var posX: Int
+        for(i in 0..squaresY) {
+            posX = 0
+            for(j in 0..squaresX) {
                 squareArray =
                     if((i + j) % 2 == 0) {
                         squareArray.plus(SquareField(posX.toFloat(), posY.toFloat(), squareWidth, squareHeight, BitmapFactory.decodeResource(resources, R.drawable.square_green)))
                     } else {
                         squareArray.plus(SquareField(posX.toFloat(), posY.toFloat(), squareWidth, squareHeight, BitmapFactory.decodeResource(resources, R.drawable.square_lightgreen)))
                     }
-                posY += squareHeight
-                squareId++
+                posX += squareWidth
             }
-            posX += squareWidth
+            posY += squareHeight
         }
     }
 }
