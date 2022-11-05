8328b4c5d5341d7e70a22d9571f1267aef1f1ed8
Robin Patzak <robinpatzak@gmail.com>
2022-11-05 16:55:04 +0100

    refactored code and added inc and dec functions

--- app/src/main/java/de/mow2/towerdefense/model/util/Vector2D.kt
+++ app/src/main/java/de/mow2/towerdefense/model/util/Vector2D.kt
@@ -1,25 +1,20 @@
 package de.mow2.towerdefense.model.util
 
-class Vector2D(val x: Float, val y: Float) {
-    constructor(x: Int, y: Int) : this(x.toFloat(), y.toFloat())
+/**
+ * Helper class to define mathematical 2D-Vectors which hold a x and y variable
+ * @param x x-coordinate of the vector
+ * @param y y-coordinate of the vector
+ * */
 
-    operator fun Vector2D.plus(other: Vector2D) : Vector2D {
-        return Vector2D(x + other.x, y + other.y)
-    }
+data class Vector2D(val x: Int, val y: Int) {
+    constructor(x: Float, y: Float) : this(x.toInt(), y.toInt())
+    constructor(x: Double, y: Double) : this(x.toInt(), y.toInt())
 
-    operator fun Vector2D.minus(other: Vector2D) : Vector2D {
-        return Vector2D(x - other.x, y - other.y)
-    }
-
-    operator fun Vector2D.times(other: Vector2D) : Vector2D {
-        return Vector2D(x * other.x, y * other.y)
-    }
-
-    operator fun Vector2D.div(other: Vector2D) : Vector2D {
-        return Vector2D(x / other.x, y / other.y)
-    }
-
-    operator fun Vector2D.rem(other: Vector2D) : Vector2D {
-        return Vector2D(x % other.x, y % other.y)
-    }
+    operator fun Vector2D.plus(other: Vector2D) = Vector2D(x + other.x, y + other.y)
+    operator fun Vector2D.minus(other: Vector2D) = Vector2D(x - other.x, y - other.y)
+    operator fun Vector2D.times(other: Vector2D) = Vector2D(x * other.x, y * other.y)
+    operator fun Vector2D.div(other: Vector2D) = Vector2D(x / other.x, y / other.y)
+    operator fun Vector2D.rem(other: Vector2D) = Vector2D(x % other.x, y % other.y)
+    operator fun Vector2D.inc() = Vector2D(x + 1, y + 1)
+    operator fun Vector2D.dec() = Vector2D(x - 1, y - 1)
 }
