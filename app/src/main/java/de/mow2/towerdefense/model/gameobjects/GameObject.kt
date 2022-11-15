5d570cae6980e157bbc1ef4f823d24121e0ff6fe
Armin <61236723+dastrinken@users.noreply.github.com>
2022-11-15 16:25:45 +0100

    Update GameObject.kt

--- app/src/main/java/de/mow2/towerdefense/model/gameobjects/GameObject.kt
+++ app/src/main/java/de/mow2/towerdefense/model/gameobjects/GameObject.kt
@@ -43,8 +43,6 @@ abstract class GameObject() {
             }
         }
     var waitUpdates: Float = 0f
-    fun positionX(): Float{ return coordX }
-    fun positionY(): Float{ return coordY }
 
     abstract fun update()
 
